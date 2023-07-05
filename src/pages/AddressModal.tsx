a27e6ccf82e2f82edf314d3d8de5f7be1fa348ae
Ranjit Kalita <ranjitkalita499@gmail.com>
2023-07-06 02:19:37 +0530

    changes

--- src/pages/AddressModal.tsx
+++ src/pages/AddressModal.tsx
@@ -6,7 +6,67 @@ interface AddressModalProps {
   onSave: (address: string) => void;
 }
 
-const AddressModal: React.FC<AddressModalProps> = ({ onClose, onSave }) => {
+interface SearchBarProps {
+  onSearch: (pincode: string, query: string) => void;
+}
+
+const validPincodes = [
+  "786126",
+  "786145",
+  "786146",
+  "786147",
+  "786148",
+  "786150",
+  "786151",
+  "786152",
+  "786153",
+  "786154",
+  "786155",
+  "786156",
+  "786157",
+  "786158",
+  "786159",
+  "786160",
+  "786170",
+  "786171",
+  "786173",
+  "786174",
+  "786179",
+  "786181",
+  "786182",
+  "786183",
+  "786187",
+  "786188",
+  "786189",
+  "786190",
+  "786601",
+];
+
+const AddressModal: React.FC<AddressModalProps & SearchBarProps> = ({
+  onClose,
+  onSave,
+  onSearch,
+}) => {
+  const [pincode, setPincode] = useState("");
+  const [query, setQuery] = useState("");
+
+  const handlePincodeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedPincode = event.target.value;
+    setPincode(selectedPincode);
+
+    if (!validPincodes.includes(selectedPincode)) {
+      alert("Select a valid pincode");
+    }
+  };
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(pincode, query);
+  };
+
   const [address, setAddress] = useState("");
 
   const handleAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -48,12 +108,18 @@ const AddressModal: React.FC<AddressModalProps> = ({ onClose, onSave }) => {
             value={address}
             onChange={handleAddressChange}
           />
-          <input
-            type="text"
-            placeholder="Pincode"
-            value={address}
-            onChange={handleAddressChange}
-          />
+          <select
+            className="pinsearch11"
+            value={pincode}
+            onChange={handlePincodeChange}
+          >
+            <option value="">Select Pincode</option>
+            {validPincodes.map((pin) => (
+              <option key={pin} value={pin}>
+                {pin}
+              </option>
+            ))}
+          </select>
           <input
             type="text"
             placeholder="City"
